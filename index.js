c203db5c3e1b773600fde1dc2df5ea94cd284d4f
James Hassett <jhassett189@gmail.com>
2022-06-13 17:03:57 -0400

    commit tree 2e15f1ac5956f7aab39e014898fb044f4fac57d1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+    return [drivers[0], drivers[1]]
+}
+const returnLastTwoDrivers = function (lastDrivers) {
+    return [lastDrivers[2], lastDrivers[3]]
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(a) {
+    return function (fare) {
+        return fare * a
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, callback) {
+    return callback(drivers)
+}
